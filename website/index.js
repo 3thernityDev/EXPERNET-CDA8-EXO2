fa8174e44635327c15ead71c6ac7cc1725b2326c
3thernitydev <3thernitydev@gmail.com>
2024-10-23 14:05:48 +0400

    add some styles and scripts for checkup form

--- website/index.js
+++ website/index.js
@@ -2,6 +2,33 @@
 const changeMe2 = (e) => {
     const txt = document.getElementById("changeMe");
     const buttonValue = txt.value;
-    document.getElementById("buttonChangeMe").innerText = buttonValue
+    document.getElementById("buttonChangeMe").innerText = buttonValue;
     console.log(buttonValue);
+};
+
+const validate = () => {
+    let fname = document.getElementById("fname");
+    let lname = document.getElementById("lname");
+    let email = document.getElementById("email");
+    let pseudo = document.getElementById("pseudo");
+    let password = document.getElementById("password");
+    let checkpassword = document.getElementById("checkpassword");
+
+    if (
+        (!fname.value,
+        !lname.value,
+        !email.value,
+        !pseudo.value,
+        !password.value,
+        !checkpassword.value)
+    ) {
+        console.log("Merci de remplir tout les champs");
+    } else {
+        if (password === checkpassword) {
+            console.log("ok pour envoie");
+        } else {
+            document.getElementById("errorPassword").innerText =
+                "Les mots de passe ne sont pas identique";
+        }
+    }
 };
